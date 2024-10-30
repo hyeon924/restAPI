f737a00bf30a424c2c342536e86bb0e3d508e2d0
hyeon924 <hyeonn4713@gmail.com>
2024-10-30 09:52:56 +0900

    adD

---
+++ src/main/java/com/example/demo/article/service/ArticleService.java
@@ -0,0 +1,55 @@
+package com.example.demo.article.service;
+
+import com.example.demo.article.dto.ArticleDTO;
+import com.example.demo.article.entity.Article;
+import com.example.demo.article.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+    public List<ArticleDTO> getList() {
+        List<Article> articleList = this.articleRepository.findAll();
+
+        List<ArticleDTO> articleDTOList = articleList.stream()
+                .map(article -> new ArticleDTO(article))
+                .collect(Collectors.toList());
+
+        return articleDTOList;
+    }
+
+    public Article getArticle(Long id) {
+        Optional<Article> optionalArticle = this.articleRepository.findById(id);
+
+        return optionalArticle.orElse(null);
+    }
+
+    public Article write(String subject, String content) {
+        Article article = Article.builder()
+                .subject(subject)
+                .content(content)
+                .build();
+        this.articleRepository.save(article);
+
+        return article;
+    }
+
+    public Article update(Article article, String content, String subject) {
+        article.setSubject(subject);
+        article.setContent(content);
+
+        this.articleRepository.save(article);
+
+        return article;
+    }
+
+    public void delete(Article article) {
+        this.articleRepository.delete(article);
+    }
+}
