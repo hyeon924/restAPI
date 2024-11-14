af2943e1f38a835a6e4c2c94dffc519e3cf76af0
hyeon924 <hyeonn4713@gmail.com>
2024-11-15 01:16:23 +0900

    JWT token 인가처리

--- src/main/java/com/example/demo/global/security/ApiSecurityConfig.java
+++ src/main/java/com/example/demo/global/security/ApiSecurityConfig.java
@@ -8,11 +8,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class ApiSecurityConfig {
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
+
     @Bean
     SecurityFilterChain apifilterChain(HttpSecurity http) throws Exception {
         http
@@ -23,7 +26,8 @@ public class ApiSecurityConfig {
                                 .requestMatchers(HttpMethod.GET, "/api/*/articles/*").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/api/*/members/join").permitAll() // 회원가입은 누구나 가능, post 요청만 허용
                                 .requestMatchers(HttpMethod.POST, "/api/*/members/login").permitAll() // 로그인은 누구나 가능, post 요청만 허용
-                                .requestMatchers(HttpMethod.GET, "/api/*/members/me").permitAll() //
+                                .requestMatchers(HttpMethod.GET, "/api/*/members/me").permitAll() // 임시로 열어놓음
+                                .requestMatchers(HttpMethod.GET, "/api/*/members/logout").permitAll() // 로그아웃은 누구나 가능, post 요청만 허용
                                 .anyRequest().authenticated()
                 )
                 .csrf(
@@ -38,6 +42,10 @@ public class ApiSecurityConfig {
                 ) // 폼 로그인 방식 끄기
                 .sessionManagement(
                         sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(
+                        jwtAuthorizationFilter, //엑세스 토큰을 이용한 로그인 처리
+                        UsernamePasswordAuthenticationFilter.class
                 );
         ;
         return http.build();
