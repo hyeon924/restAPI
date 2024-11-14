c3a43c1f5619fbafeb69bcabd30f58a2157b8bc8
hyeon924 <hyeonn4713@gmail.com>
2024-11-15 00:06:50 +0900

    member setting

---
+++ src/main/java/com/example/demo/domain/member/entity/Member.java
@@ -0,0 +1,18 @@
+package com.example.demo.domain.member.entity;
+
+import com.example.demo.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class Member extends BaseEntity {
+    private String username;
+    private String password;
+}
