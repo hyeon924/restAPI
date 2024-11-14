af88f72a1b13563296790bb66cee3cde8df9a703
hyeon924 <hyeonn4713@gmail.com>
2024-11-15 00:23:10 +0900

    m security

--- src/main/java/com/example/demo/domain/member/service/MemberService.java
+++ src/main/java/com/example/demo/domain/member/service/MemberService.java
@@ -3,18 +3,20 @@ package com.example.demo.domain.member.service;
 import com.example.demo.domain.member.entity.Member;
 import com.example.demo.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
 //  회원 정보 저장(등록)
 public void join(String username, String password) {
     Member member = Member.builder()
             .username(username)
-            .password(password)
+            .password(passwordEncoder.encode(password))
             .build();
     this.memberRepository.save(member);
 }
